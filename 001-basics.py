10672391c68127c39769d3a4aaf576724b195202
Tahirov, Jalil <jtahirov+deloitte@deloitte.com>
2025-12-26 19:03:49 +0100

    added methods

--- 001-basics.py
+++ 001-basics.py
@@ -1,3 +1,20 @@
-print('Hello World!')
-print('it works')
+def print_hello():
+  print('Hello World!')
+  print('it works')
+  arr =  [1,2,3,4,5]
+  for i in arr:
+    print(i + 10)
+
+def print_items(n):
+    print('items before ')
+    while n>1:
+      print(n)
+      n-=1
+
+ 
+print_items(4)
+
+#print_items(90)
+
+
 
