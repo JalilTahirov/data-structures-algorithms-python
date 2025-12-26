929e137b6db2be446015b2433dce5c4ba070218d
Tahirov, Jalil <jtahirov+deloitte@deloitte.com>
2025-12-26 20:02:02 +0100

    O n square was added

--- 001-basics.py
+++ 001-basics.py
@@ -7,18 +7,25 @@ def print_hello():
 
 def print_itemsone(n):
     print('items before ')
-    while n>1:
+    while n>=0:
       print(n)
       n-=1
-
+# O(n)
 def print_items(n):
    for i in range(n):
       print(i)
+
+# O(n)
+def print_items_two(n):
+   for i in range(n):
+      for j in range(n):
+        print(i,j)
     
  
-print_items(4)
+#print_items(10)
 
-#print_items(90)
+print_items_two(3)
+
 
 
 
